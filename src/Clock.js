55ec080493aee12a0752f44601ab2ce94d67b400
Ali <ali.dahlman57@gmail.com>
2020-02-12 12:26:40 -0500

    space bar pause/unpause

--- src/Clock.js
+++ src/Clock.js
@@ -1,43 +1,48 @@
 import React from "react";
 
 import Typography from "@material-ui/core/Typography";
-import { Button, Grid, Paper } from "@material-ui/core";
+import { Box, Button, Grid, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import leadingZero from "./functions";
 
 import "./Clock.scss";
 
 const useStyles = makeStyles(theme => ({
-  // root: {
-  //   margin: theme.spacing(6, 0, 3),
-  // },
-  // lightBulb: {
-  //   verticalAlign: 'middle',
-  //   marginRight: theme.spacing(1),
-  // },
   clockFace: {
     padding: theme.spacing(2)
   },
   running: {
     backgroundColor: theme.palette.success.main
   },
-  paused: {
-    backgroundColor: theme.palette.warning.light
+  resting: {
+    backgroundColor: theme.palette.warning.light,
+    span: {}
   },
-  ended: {
+  ending: {
     background: theme.palette.error.main
+  },
+  "@keyframes flash": {
+    "0%": { opacity: 1 },
+    "50%": { opacity: 0.1 },
+    "100%": { opacity: 1 }
+  },
+  paused: {
+    animation: "$flash 2s linear infinite"
   }
 }));
 
 function ClockFace(props) {
   const classes = useStyles();
-  const timeRemaining = props.timeRemaining;
-  const minutes = leadingZero(toMinutes(timeRemaining));
-  const seconds = leadingZero(toSeconds(timeRemaining));
+  const minutes = leadingZero(toMinutes(props.timeRemaining));
+  const seconds = leadingZero(toSeconds(props.timeRemaining));
+  const status = props.status;
+  console.log(props);
   return (
-    <Paper className={props.status ? classes.running : classes.paused}>
+    <Paper className={status ? classes[status] : ""}>
       <h3 className='digital'>
-        {minutes}:{seconds}
+        <span className={props.paused ? classes.paused : ""}>
+          {minutes}:{seconds}
+        </span>
       </h3>
     </Paper>
   );
@@ -51,15 +56,46 @@ export default class Clock extends React.Component {
       matchEnded: props.matchEnded,
       running: false,
       started: false,
-      waiting: true
+      warning: false,
+      waiting: false
     };
     this.handleToggle = this.handleToggle.bind(this);
     this.handleReset = this.handleReset.bind(this);
+    this.handleResetAll = this.handleResetAll.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+    clearInterval(this.timer);
+  }
+  handleKeyPress(event) {
+    if (event.keyCode === 32 && !this.props.matchEnded && !this.state.waiting) {
+      this.handleToggle();
+    }
+  }
+  handleResetAll() {
+    clearInterval(this.timer);
+    this.setState({
+      timeRemaining: this.props.timeRemaining,
+      started: false,
+      running: false
+    });
+    this.props.lockSettings(true);
+  }
   handleToggle() {
     const running = this.state.running;
-    this.props.lockSettings();
+    const started = this.state.started;
+    if (!started) {
+      this.setState({
+        started: true,
+        timeRemaining: this.props.timeRemaining
+      });
+      this.props.lockSettings(false);
+    }
     if (!this.props.matchEnded) {
       running ? this.stop() : this.start();
     }
@@ -74,30 +110,27 @@ export default class Clock extends React.Component {
   }
 
   beginRound() {
-    console.log("begin");
-
     if (this.props.countDown) {
-      console.log("countdown", this.state.running);
       clearInterval(this.timer);
       this.setState({
-        timeRemaining: this.props.timeRemaining
-        // started: true,
+        timeRemaining: this.props.timeRemaining,
+        warning: false,
+        running: false,
+        waiting: true
       });
       setTimeout(() => {
+        this.setState({ waiting: false });
         this.start();
       }, 2000);
     } else {
-      console.log("no countdown");
       this.start();
     }
   }
   start() {
     if (!this.state.running && !this.props.matchEnded) {
-      // if (!this.state.timeRemaining) {
-      //   this.setState({ timeRemaining: this.props.timeRemaining });
-      // }
       this.setState({
-        running: true
+        running: true,
+        warning: false
       });
       this.timer = setInterval(this.tick, 1000);
     }
@@ -108,48 +141,88 @@ export default class Clock extends React.Component {
   }
   tick = () => {
     let seconds = this.state.timeRemaining;
+    if (seconds === 2000) {
+      this.setState({
+        warning: true
+      });
+    }
     if (seconds <= 0) {
       this.props.incrementRound();
       this.stop();
       if (!this.props.matchEnded) {
         this.setState({ timeRemaining: this.props.timeRemaining });
+        this.beginRound();
       }
-      this.beginRound();
-
-      // this.start();
       return;
     }
     this.setState({
       timeRemaining: seconds - 1000
     });
   };
+
+  getStatus() {
+    if (this.state.warning && this.props.countDown) {
+      return "ending";
+    }
+    if (this.state.started) {
+      if (!this.props.countDown) {
+        return "resting";
+      } else if (this.state.running) return "running";
+    } else {
+      return "";
+    }
+  }
   render() {
     const running = this.state.running;
+    const timeRemaining = this.state.started
+      ? this.state.timeRemaining
+      : this.props.timeRemaining;
+    const status = this.getStatus();
+    console.log(this.state);
 
     return (
       <div>
-        {this.props.countDown ? "waiting" : "regular"}
-        <Grid container direction='row' justify='center' alignItems='center'>
-          <Grid item xs={12}>
-            <ClockFace
-              timeRemaining={this.state.timeRemaining}
-              status={this.state.running}
-            />
-          </Grid>
-          <Grid item xs={6}>
+        <ClockFace
+          timeRemaining={timeRemaining}
+          status={status}
+          paused={
+            !this.state.running && this.state.started && !this.props.matchEnded
+          }
+        />
+        <Grid
+          container
+          direction='row'
+          justify='space-between'
+          alignItems='center'>
+          <Grid item xs={3}>
             <Button
               variant='contained'
               color='primary'
-              disabled={this.props.matchEnded}
+              disabled={this.props.matchEnded || this.state.waiting}
               onClick={this.handleToggle}>
               {running ? "Pause" : "Start"}
             </Button>
           </Grid>
-          <Grid item xs={6}>
-            <Button variant='contained' onClick={this.handleReset}>
-              Restart Round
-            </Button>
+          <Grid item xs={3}>
+            {!this.state.started ? null : (
+              <Button
+                variant='contained'
+                onClick={this.handleResetAll}
+                disabled={this.state.waiting}>
+                Restart all
+              </Button>
+            )}
           </Grid>
+          {/* <Grid item xs={4}>
+            {!this.state.started ? null : (
+              <Button
+                variant='contained'
+                onClick={this.handleReset}
+                disabled={this.state.waiting}>
+                Restart Round
+              </Button>
+            )}
+          </Grid> */}
         </Grid>
       </div>
     );
